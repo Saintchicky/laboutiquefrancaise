bb78a3e72af7c3405b087fa6c0a8ea4056889f4f
Saintchicky <poussin6494@hotmail.com>
2021-09-27 18:44:30 +0200

    [IMP] mdp maj

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,26 +2,17 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AccountController extends AbstractController
 {
     /**
      * @Route("/compte", name="account")
      */
-    public function index(Request $request, UserPasswordEncoderInterface $encoder): Response
+    public function index(): Response
     {
-        $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()){
-            $old_pwd = $form->get('old_password')->getData();
-            if($encoder->isPasswordValid($user,$old_pwd)){
-                
-            }
-        }
         return $this->render('account/index.html.twig');
     }
 }
--- src/Controller/AccountPasswordController.php
+++ src/Controller/AccountPasswordController.php
@@ -3,22 +3,45 @@
 namespace App\Controller;
 
 use App\Form\ChangePasswordType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AccountPasswordController extends AbstractController
 {
+    // Déclarer Doctrine
+    private $entityManager;
+    public function __construct(EntityManagerInterface $entityManager){
+        $this->entityManager = $entityManager;
+    }
     /**
      * @Route("/compte/modifier-mon-mot-passe", name="account_password")
      */
-    public function index(): Response
+    public function index(Request $request, UserPasswordEncoderInterface $encoder): Response
     {
-        // Recupère l'user connecté
+        $notification = null;
         $user = $this->getUser();
         $form = $this->createForm(ChangePasswordType::class, $user);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $old_pwd = $form->get('old_password')->getData();
+            if($encoder->isPasswordValid($user,$old_pwd)){
+                $new_pwd = $form->get('new_password')->getData();
+                $password = $encoder->encodePassword($user,$new_pwd);
+                $user->setPassword($password);
+                // pas besoin de figer la donnée  avec doctrine car update
+                $this->entityManager->flush();
+                $notification = "Votre mot de passe a bien été mis à jour";
+            }else{
+                $notification = "Votre mot de passe actuel n'est pas le bon";
+            }
+        }
         return $this->render('account/password.html.twig',[
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'notification'=>$notification
         ]);
     }
 }
