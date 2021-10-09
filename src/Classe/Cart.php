3928950f307d9bb0ef1352e92d9955889ac94817
Saintchicky <poussin6494@hotmail.com>
2021-10-09 19:00:02 +0200

    [IMP] panier done

--- src/Classe/Cart.php
+++ src/Classe/Cart.php
@@ -2,12 +2,18 @@
 
 namespace App\Classe;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class Cart{
     private $session;
-    public function __construct(SessionInterface $session){
+    // Déclarer Doctrine
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager,SessionInterface $session){
         $this->session = $session;
+        $this->entityManager = $entityManager;
     }
     public function add($id){
         $cart = $this->session->get('cart', []);
@@ -25,5 +31,41 @@ class Cart{
     }
     public function remove(){
         return $this->session->remove('cart');
+    }
+    public function delete($id){
+        $cart = $this->session->get('cart', []);
+        unset($cart[$id]);
+        return $this->session->set('cart',$cart);
+    }
+    public function decrease($id){
+        $cart = $this->session->get('cart', []);
+        // si quantité supp a 1 on décremente
+        if($cart[$id] > 1){
+            $cart[$id] --;
+        }else{
+            // si egal à 1 alors on supprime
+            unset($cart[$id]);
+        }
+        return $this->session->set('cart',$cart);
+    }
+    public function getFull(){
+        $cartComplete = [];
+        // si le panier n'est pas vide
+        if($this->get()){
+            foreach($this->get() as $id =>$quantity){
+                $product_object = $this->entityManager->getRepository(Product::class)->findOneById($id);
+                // si aucun produit trouvé on supprime
+                if(!$product_object){
+                    $this->delete($id);
+                    // et on sort de la boucle
+                    continue;
+                }
+                $cartComplete[] = [
+                    'product' => $product_object,
+                    'quantity' =>$quantity,
+                ];
+            }
+            return $cartComplete;
+        }
     }
 }
--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,33 +3,19 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
-use App\Entity\Product;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CartController extends AbstractController
 {
-    // Déclarer Doctrine
-    private $entityManager;
-    public function __construct(EntityManagerInterface $entityManager){
-        $this->entityManager = $entityManager;
-    }
     /**
-     * @Route("/mon-panier", name="cart")
-     */
+    * @Route("/mon-panier", name="cart")
+    */
     public function index(Cart $cart): Response
     {
-        $cartComplete = [];
-        foreach($cart->get() as $id =>$quantity){
-            $cartComplete[] = [
-                'product' => $this->entityManager->getRepository(Product::class)->findOneById($id),
-                'quantity' =>$quantity,
-            ];
-        }
         return $this->render('cart/index.html.twig',[
-            'cart' => $cartComplete
+            'cart' => $cart->getFull()
         ]);
     }
     /**
@@ -40,12 +26,28 @@ class CartController extends AbstractController
         $cart->add($id);
         return $this->redirectToRoute('cart');
     }
-        /**
-     * @Route("/cart/remove", name="remove_my_cart")
-     */
+    /**
+    * @Route("/cart/remove", name="remove_my_cart")
+    */
     public function remove(Cart $cart): Response
     {
         $cart->remove();
         return $this->redirectToRoute('products');
+    }
+    /**
+    * @Route("/cart/delete/{id}", name="delete_to_cart")
+    */
+    public function delete(Cart $cart, $id): Response
+    {
+        $cart->delete($id);
+        return $this->redirectToRoute('cart');
+    }
+    /**
+    * @Route("/cart/decrease/{id}", name="decrease_to_cart")
+    */
+    public function decrease(Cart $cart, $id): Response
+    {
+        $cart->decrease($id);
+        return $this->redirectToRoute('cart');
     }
 }
